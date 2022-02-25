637052112e4d85d92220d5acfa99af7b21586b94
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-02-26 00:44:29 +0600

    api added

---
+++ js/foodDB.js
@@ -0,0 +1,30 @@
+document.getElementById('error-message').style.display = 'none';
+const searchFood = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    //clear search field
+    searchField.value = '';
+    // data load
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data.meals));
+}
+
+const displaySearchResult = meals => {
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    })
+}
